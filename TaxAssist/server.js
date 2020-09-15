b2f312d8bb10d05cc1b9d1c8cce4f4299b57009a
SaifAlsukhni <70297354+SaifAlsukhni@users.noreply.github.com>
2020-09-15 02:24:24 -0500

    3

--- TaxAssist/server.js
+++ TaxAssist/server.js
@@ -9,16 +9,32 @@ let app = http.createServer((request, response) => {
     if (filePath === './')
         filePath = './public/views/index.html'
 
-    fs.readFile(filePath, function (error, content) {
-       if (error) {
-            response.writeHead(404)
-            response.end()
-        } else {
-            response.writeHead(200, {'Content-Type': 'text/html' })
-            response.end(content, 'utf-8')
-        }
+    // Checks for the filePath extension and creates a variable for the Content type
+    let extname = String(path.extname(filePath))
+    let fileType = {
+        '.html': 'text/html',
+        '.css': 'text/css',
+        '.js': 'text/js',
+        '.png': 'image/png'
+    }
+    let contentType = fileType[extname]
 
-    })
+    // If-statement that checks for the file existing
+    if (fs.existsSync('./public/views/index.html')) {
+        fs.readFile(filePath, function (error, content) {
+            if (error) {
+                response.writeHead(500)
+                response.end()
+            } else {
+                response.writeHead(200, {'Content-Type': contentType})
+                response.end(content, 'utf-8')
+            }
+
+        })
+    } else {
+        response.writeHead(404)
+        response.end()
+    }
 })
 app.listen(3000)
 console.log('Server is running at 127.0.0.1:3000/ or http://localhost:3000')
