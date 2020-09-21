92a60b9f315a8864efe7b05329a7c54dc88d68f4
SaifAlsukhni <70297354+SaifAlsukhni@users.noreply.github.com>
2020-09-20 23:51:05 -0500

    4

--- TaxAssist/public/js/welcome.js
+++ TaxAssist/public/js/welcome.js
@@ -1,3 +1,12 @@
+let { DateTime } = require('luxon')
+
+let localTime = DateTime.local().toLocaleString(DateTime.DATETIME_FULL)
+let dueDate = DateTime.local(2020, 10, 18, 23, 59).toLocaleString(DateTime.DATETIME_FULL)
+
 exports.welcomeToTax = function () {
     console.log("Welcome to TaxAssist!")
+    console.log('It is currently: ' + localTime)
+    console.log('Please submit your data before: ' + dueDate)
+    console.log("Thank you for using TaxAssist!")
+
 }
