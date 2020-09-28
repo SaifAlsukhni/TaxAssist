d95a9f22736612df092f562fbc68697f55ef33cf
SaifAlsukhni <70297354+SaifAlsukhni@users.noreply.github.com>
2020-09-28 16:31:04 -0500

    5

--- TaxAssist/public/js/deductions.js
+++ TaxAssist/public/js/deductions.js
@@ -1,10 +1,18 @@
 let receipts = require('./receipts')
+let EventEmitter = require('events').EventEmitter
 
 exports.addDeductions = function () {
     console.log("Deduction amounts are entered here.")
-    let receipt = receipts.addReceipts()
 
 
+    let emitter = new EventEmitter()
+    let receipt = receipts.addReceipts(emitter)
 
-
+    let counter = 1
+    while (counter <= 10) {
+        console.log('Pre-emitter ' + counter)
+        emitter.emit('event', counter)
+        console.log('Post-emitter ' + counter)
+        counter++
+    }
 }
--- TaxAssist/public/js/welcome.js
+++ TaxAssist/public/js/welcome.js
@@ -1,12 +1,19 @@
 let { DateTime } = require('luxon')
 
-let localTime = DateTime.local().toLocaleString(DateTime.DATETIME_FULL)
-let dueDate = DateTime.local(2020, 10, 18, 23, 59).toLocaleString(DateTime.DATETIME_FULL)
+let localTime = DateTime.local().toLocaleString(DateTime.DATETIME_HUGE)
+let dueDate = DateTime.local(2020, 10, 18, 23, 59).toLocaleString(DateTime.DATETIME_HUGE)
+
+let localTime1 = DateTime.local()
+let dueDate1 = DateTime.local(2020, 10, 18, 23, 59)
+
+let timeLeft = dueDate1.diff(localTime1, ['days', 'hours', 'minutes', 'seconds']).toObject()
+JSON.stringify(timeLeft)
 
 exports.welcomeToTax = function () {
     console.log("Welcome to TaxAssist!")
     console.log('It is currently: ' + localTime)
     console.log('Please submit your data before: ' + dueDate)
+    console.log('You have: ' + timeLeft.days + ' days, ' + timeLeft.hours + ' hours, ' +  timeLeft.minutes + ' minutes, and ' + timeLeft.seconds + ' seconds' + ' to submit your data.')
     console.log("Thank you for using TaxAssist!")
 
 }
